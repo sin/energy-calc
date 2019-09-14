44d4f42b44989ff1a489998aa0d1dbb5f1929735
Mateusz Sienkan <sin.mateusz@gmail.com>
2019-09-14 11:07:08 +0200

    Updates

---
+++ precache-manifest.36f887a587dbcb6c49ff969ff9bc241e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed856b6db6ed2ce142f6a698c538ee3",
+    "url": "/energy-calc/index.html"
+  },
+  {
+    "revision": "cdcb676e907bae761973",
+    "url": "/energy-calc/static/css/2.8b1b5f58.chunk.css"
+  },
+  {
+    "revision": "bff21124ea2a9c5129c3",
+    "url": "/energy-calc/static/css/main.8e07387f.chunk.css"
+  },
+  {
+    "revision": "cdcb676e907bae761973",
+    "url": "/energy-calc/static/js/2.5525c2ce.chunk.js"
+  },
+  {
+    "revision": "bff21124ea2a9c5129c3",
+    "url": "/energy-calc/static/js/main.927ac6f2.chunk.js"
+  },
+  {
+    "revision": "06dfcfb69d01ee0168ae",
+    "url": "/energy-calc/static/js/runtime~main.d4e8ddea.js"
+  }
+]);
