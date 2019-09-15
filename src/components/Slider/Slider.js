229613754b28539abb9c302c1b6480949ff10f7f
Mateusz Sienkan <sin.mateusz@gmail.com>
2019-09-15 02:31:00 +0200

    Style and refactor Slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import InputRange from 'react-input-range'
+
 import 'react-input-range/lib/css/index.css'
 import './Slider.css'
 
-export default function Slider(props) {
+export default function Slider({ name, value, update }) {
   return (
-    <div className="slider">
-      <p>{`${props.name}: ${props.value}`}</p>
-      <InputRange
-        maxValue={25000}
-        minValue={0}
-        step={500}
-        value={props.value}
-        onChange={props.update}
-      />
+    <div className="Slider">
+      <div className="Slider__label">
+        <div className="Slider__name">{name}</div>
+        <div className="Slider__value">
+          {value}
+          <span className="Slider__unit">{' MW'}</span>
+        </div>
+      </div>
+      <InputRange maxValue={25000} minValue={0} step={500} value={value} onChange={update} />
     </div>
   )
 }
