8c2475ee28a44486cd2ca1124f964e767f2f7d53
Mateusz Sienkan <sin.mateusz@gmail.com>
2019-09-19 06:45:44 +0200

    Updates

---
+++ precache-manifest.2a458345928d7aaee698ed346b0dc9b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d500d25a1ea6860ae5c0757d4a73393",
+    "url": "/energy-calc/index.html"
+  },
+  {
+    "revision": "c770483de53182b5182b",
+    "url": "/energy-calc/static/css/2.8b1b5f58.chunk.css"
+  },
+  {
+    "revision": "e44d19ef96fac5299a14",
+    "url": "/energy-calc/static/css/main.34d4e920.chunk.css"
+  },
+  {
+    "revision": "c770483de53182b5182b",
+    "url": "/energy-calc/static/js/2.77138b88.chunk.js"
+  },
+  {
+    "revision": "e44d19ef96fac5299a14",
+    "url": "/energy-calc/static/js/main.a19f794a.chunk.js"
+  },
+  {
+    "revision": "06dfcfb69d01ee0168ae",
+    "url": "/energy-calc/static/js/runtime~main.d4e8ddea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/energy-calc/precache-manifest.84c2fbf1581e710a20d19d0f1c7c923b.js"
+  "/energy-calc/precache-manifest.2a458345928d7aaee698ed346b0dc9b8.js"
 );
 
 self.addEventListener('message', (event) => {
